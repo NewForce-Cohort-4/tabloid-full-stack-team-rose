964aa9a27a977f652cfaa0f7f8053b8efdeefbaf
Gabriel <bultaqi@gmail.com>
2021-06-08 15:46:48 -0400

    yay! Still a broken API. I am now working on implementing the methods that stores the post as an object

---
+++ Tabloid/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Tabloid.Repositories
+{
+    internal class Category
+    {
+        public Category()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
