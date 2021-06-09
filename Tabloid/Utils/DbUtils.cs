e2999ed35b6572b2c6d5fc782179b50e991fd9ab
Gabriel <bultaqi@gmail.com>
2021-06-09 10:11:38 -0400

    working on getting current user and merging all posts

--- Tabloid/Utils/DbUtils.cs
+++ Tabloid/Utils/DbUtils.cs
@@ -83,6 +83,16 @@ namespace Tabloid.Utils
             return reader.GetDateTime(ordinal);
         }
 
+        public static string GetNullableString(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+            return reader.GetString(ordinal);
+        }
+
         /// <summary>
         ///  Determine if the value a given column is NULL
         /// </summary>
