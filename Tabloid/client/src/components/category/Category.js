cd40ffed29f814731587ac5aed41b1a068fff3ec
Detective-Coder <aaronfrankenfield@gmail.com>
2021-06-14 16:05:23 -0400

    adding delete categories on client side

--- Tabloid/client/src/components/category/Category.js
+++ Tabloid/client/src/components/category/Category.js
@@ -1,10 +1,45 @@
-import React from "react";
+// export default function Category(category) {
+//     return(
+//       <Card className="m-4">
+//         <p className="text-left px-2">{category.category.name}</p>
+//       </Card>
+//     );
+// }
+
+import React, { useContext, useState } from "react";
 import { Card, CardImg, CardBody } from "reactstrap";
+import { CategoryContext } from "../../providers/CategoryProvider"
+import { useHistory } from "react-router-dom";
 
 export default function Category(category) {
-    return(
+  const history = useHistory();
+  const { deleteCategory } = useContext(CategoryContext)
+
+
+  const handleDelete = () => {
+    const r = window.confirm(`Are you sure you want to delete "${category.category.name}"?`)
+    if (r === true) {
+      deleteCategory(category.category.id)
+        .then(() => {
+          history.push("/category")
+        })
+    }
+    else
+    {
+      history.push("/category")
+    }
+
+  }
+    return (
+      category.category.post.length !== 0 ?
       <Card className="m-4">
         <p className="text-left px-2">{category.category.name}</p>
+      </Card> 
+      :   
+      <Card className="m-4">
+        <p className="text-left px-2">{category.category.name}</p>
+        <button onClick={handleDelete}>Delete</button>
       </Card>
     );
+
 }
