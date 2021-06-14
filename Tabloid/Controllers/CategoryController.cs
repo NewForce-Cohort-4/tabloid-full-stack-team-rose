b01e5b30fda1ef480cb4a954fa3f4459167aecf6
Detective-Coder <aaronfrankenfield@gmail.com>
2021-06-14 09:51:07 -0400

    first commit

---
+++ Tabloid/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Models;
+using Tabloid.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tabloid.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_categoryRepository.GetAll());
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //// POST api/<CategoryController>
+        [HttpPost]
+        public void Post(Category category)
+        {
+            _categoryRepository.Add(category);
+        }
+
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
